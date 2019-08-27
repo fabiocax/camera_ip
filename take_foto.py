9ad711fc670e66a9e17a499b560a83d92c894e24
fabiocax <fabiocax@gmail.com>
2019-08-27 10:02:48 -0300

    remove teste

--- take_foto.py
+++ take_foto.py
@@ -14,7 +14,7 @@ camera = cv2.VideoCapture(0)
 while True:
     return_value,image = camera.read()
     gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
-    cv2.imwrite(IMAGE_TMP,image)
+    cv2.imwrite(IMAGE_TMP,gray)
     break
 camera.release()
 cv2.destroyAllWindows()
